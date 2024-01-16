6c9e976c0529d0d68ccf5f2a9c2a0014caaee72a
code . <blazej686@yahoo.com>
2024-01-16 12:17:03 -0700

    Docker update

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,56 +1,24 @@
--- Active: 1702937458189@@54.212.62.175@3306@keeper
+-- Active: 1705429522848@@54.214.151.71@3306@keeper
 
-CREATE TABLE
-    IF NOT EXISTS accounts(
-        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-        name varchar(255) COMMENT 'User Name',
-        email varchar(255) COMMENT 'User Email',
-        picture varchar(255) COMMENT 'User Picture',
-        coverImg VARCHAR(255)
-    ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key', createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created', updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update', name varchar(255) COMMENT 'User Name', email varchar(255) COMMENT 'User Email', picture varchar(255) COMMENT 'User Picture', coverImg VARCHAR(255)
+) default charset utf8 COMMENT '';
 
-CREATE TABLE
-    IF NOT EXISTS keeps(
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        creatorId VARCHAR(255) NOT NULL,
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-        name VARCHAR(255) NOT NULL,
-        description VARCHAR(500) NOT NULL,
-        img VARCHAR(500) NOT NULL,
-        views INT NOT NULL DEFAULT 0,
-        Foreign Key (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
-    ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS keeps (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, creatorId VARCHAR(255) NOT NULL, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created', updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update', name VARCHAR(255) NOT NULL, description VARCHAR(500) NOT NULL, img VARCHAR(500) NOT NULL, views INT NOT NULL DEFAULT 0, Foreign Key (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
 
-CREATE TABLE
-    IF NOT EXISTS vaults(
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        creatorId VARCHAR(255) NOT NULL,
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-        name VARCHAR(255) NOT NULL,
-        description VARCHAR(500) NOT NULL,
-        img VARCHAR(500) NOT NULL,
-        isPrivate BOOLEAN DEFAULT false
-    ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS vaults (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, creatorId VARCHAR(255) NOT NULL, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created', updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update', name VARCHAR(255) NOT NULL, description VARCHAR(500) NOT NULL, img VARCHAR(500) NOT NULL, isPrivate BOOLEAN DEFAULT false
+) default charset utf8 COMMENT '';
 
-CREATE TABLE
-    IF NOT EXISTS vaultKeeps(
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        creatorId VARCHAR(255) NOT NULL,
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-        vaultId INT NOT NULL,
-        keepId INT NOT NULL
-    ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS vaultKeeps (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, creatorId VARCHAR(255) NOT NULL, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created', updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update', vaultId INT NOT NULL, keepId INT NOT NULL
+) default charset utf8 COMMENT '';
 
-SELECT
-    keeps.*,
-    COUNT(vau.id) AS kept,
-    acc.*
-FROM keeps
+SELECT keeps.*, COUNT(vau.id) AS kept, acc.*
+FROM
+    keeps
     JOIN accounts acc ON acc.id = keeps.creatorId
     LEFT JOIN vaultKeeps vau ON vau.keepId = keeps.id -- WHERE keeps.id = @keepId
 GROUP BY (keeps.id)
